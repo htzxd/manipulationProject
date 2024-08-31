01606020daa6d068a5eb3744c3dca3604a3dc58e
Marco Pereira <marcope043@gmail.com>
2024-08-31 18:54:23 -0300

    skeleton pages

---
+++ src/pages/road/index.jsx
@@ -0,0 +1,16 @@
+import { Container, Main } from "./style";
+
+import { Header } from "../../components/header";
+import { Footer } from "../../components/footer";
+
+export function Road(){
+  return(
+    <Container>
+      <Header/>
+      <Main>
+
+      </Main>
+      <Footer/>
+    </Container>
+  )
+}
--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import { Profile } from "../pages/profile";
 import { Home } from "../pages/home";
+import { Road } from "../pages/road";
 
 export function AppRoutes(){
   return(
     <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/profile" element={<Profile />}/>
+      <Route path="/road" element={<Road />}/>
     </Routes>
   )
 }
